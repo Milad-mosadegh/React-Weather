fe618e8123323f8e59b63e47d685708bf258f02e
sdf <dfvsdfvadv>
2020-04-17 13:47:53 +0200

    Updates

---
+++ precache-manifest.68b0b80d7ddfce46b6471a5db0fc2d94.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd912bee7f5df34248401f27132f64a6",
+    "url": "/React-Weather/index.html"
+  },
+  {
+    "revision": "89f858ea3234c6d23d53",
+    "url": "/React-Weather/static/css/2.05733528.chunk.css"
+  },
+  {
+    "revision": "074b2d7af194e23f0d9e",
+    "url": "/React-Weather/static/css/main.be8c1cab.chunk.css"
+  },
+  {
+    "revision": "89f858ea3234c6d23d53",
+    "url": "/React-Weather/static/js/2.6f6a12dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather/static/js/2.6f6a12dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074b2d7af194e23f0d9e",
+    "url": "/React-Weather/static/js/main.ec6b8d32.chunk.js"
+  },
+  {
+    "revision": "a4a249d0820bd8198331",
+    "url": "/React-Weather/static/js/runtime-main.107cfa80.js"
+  },
+  {
+    "revision": "30a0430c293e7a3f082f03eb3cf429f9",
+    "url": "/React-Weather/static/media/Back.30a0430c.jpg"
+  },
+  {
+    "revision": "823cea51483aaa6a5bcc7623d8b1d4c1",
+    "url": "/React-Weather/static/media/bahal.823cea51.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Weather/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Weather/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Weather/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Weather/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Weather/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fab5a15f0f95054295139f459a073ace",
+    "url": "/React-Weather/static/media/sunny.fab5a15f.jpg"
+  }
+]);
